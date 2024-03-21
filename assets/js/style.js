d95b1fc885a28a6fa251f4430a36d7f4253cbb5e
rinklepoonia <rinklepoonia889@gmail.com>
2024-03-21 14:31:23 +0530

    fav-icon back to top preloder

--- assets/js/style.js
+++ assets/js/style.js
@@ -6,3 +6,23 @@ menuIcon.addEventListener("click", function () {
     document.body.classList.toggle("overFlowHidden")
     document.querySelector(".menuIcon").classList.toggle("cross")
 })
+// preloder
+const Preload = document.getElementById("preload")
+setTimeout(() => {
+    Preload.classList.add("d-none")
+}, "4000");
+
+// back-to-top
+const topBtn = document.querySelector(".topBtn");
+const show = document.querySelector(".show");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+        topBtn.classList.add("show");
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+});
